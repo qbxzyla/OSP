284d764ea2597f5f6a36b2b71dbb9401730cdbf4
Jakub Żyła <s38862@s.pwste.edu.pl>
2024-01-24 00:32:57 +0100

    Dodaj pliki projektów.

---
+++ OSP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OSP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
